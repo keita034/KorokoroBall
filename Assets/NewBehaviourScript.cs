79efa7a104e2d3e1af8c521e9afb3096195fd34f
keita_momiyama <93181795+keita034@users.noreply.github.com>
2022-05-10 14:39:32 +0900

    確認課題_3

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,49 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     public float power = 1;
     public new Rigidbody rigidbody;
 
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rigidbody.position.y < -20)
-        {
-            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(sceneIndex);
-        }
-        
-
         if (rigidbody.velocity.magnitude < 5)
         {
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.W))
             {
 
                 rigidbody.AddForce(new Vector3(0, 0, 1) * power);
             }
 
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.A))
             {
                 rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
             }
 
-            if (Input.GetKey(KeyCode.DownArrow))
+            if (Input.GetKey(KeyCode.S))
             {
                 rigidbody.AddForce(new Vector3(0, 0, -1) * power);
             }
 
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.D))
             {
                 rigidbody.AddForce(new Vector3(1, 0, 0) * power);
             }
